4ce335ffc1bed2be47ff5ce31b5c0f55b78b7f11
pavelitovox <ptoledopicazo@gmail.com>
2022-01-10 20:25:09 -0300

    inicio poto

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const server = http.createServer((req, res) => {
 res.end('hola mundo cruel')
 
 }); arriba sin uso de modulos ni express */
+/*                     ****************************************+
+                    token para github
+                    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
+
+                    video
+                    https://www.youtube.com/watch?v=hlYkxZrdeBI
+                    ***************************************** */
+
+
 const express = require('express');
 
 const app = express();
